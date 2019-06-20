c0ca5e8f16c98438b9f32f8cb56736826a99042c
Joshua Cheng <joshua.bj.cheng@gmail.com>
2019-06-19 19:17:37 -0700

    Add background image

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -2,12 +2,19 @@ import React, { Component } from 'react'
 import { withStyles } from "@material-ui/core/styles";
 import Footer from "../Footer";
 import logo from 'assets/logo.svg';
+import backgroundImage from 'assets/hack2020-landing-page.svg';
 
 const styles = theme => ({
-    logo: {
-        width: "200px",
-        marginTop: "20px"
-    }
+  logo: {
+    width: "200px",
+    marginTop: "20px"
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    top: 0,
+    zIndex: -1
+  }
 });
 
 export class LandingPage extends Component {
@@ -16,9 +23,10 @@ export class LandingPage extends Component {
 
     return (
       <div>
-        <img className={classes.logo} src={logo} alt="HackIllinois Logo" />
+        <img className={classes.background} src={backgroundImage} alt="Background" />
+        {/* <img className={classes.logo} src={logo} alt="HackIllinois Logo" />
         <h1>Explore. Connect. Build.</h1>
-        <h3>Feb. 28 - Mar. 1, 2020</h3>
+        <h3>Feb. 28 - Mar. 1, 2020</h3> */}
         <Footer />
       </div>
     )
