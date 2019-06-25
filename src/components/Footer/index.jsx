1991dc6954cbdb0751824b2fd0cd20f98db998eb
Joshua Cheng <joshua.bj.cheng@gmail.com>
2019-06-24 22:49:01 -0700

    Add media queries for tagline

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import envelope from 'assets/Icons/envelope.svg';
+// import envelope from 'assets/Icons/envelope.svg';
 import facebook from 'assets/Icons/facebook-f.svg';
 import github from 'assets/Icons/github.svg';
-import globe from 'assets/Icons/globe.svg';
+// import globe from 'assets/Icons/globe.svg';
 import instagram from 'assets/Icons/instagram.svg';
 import twitter from 'assets/Icons/twitter.svg';
 
@@ -14,9 +14,9 @@ const Footer = () => (
         <a href="https://www.github.com/hackillinois/" target="_blank" rel="noopener noreferrer">
             <img src={github} alt="Github Icon" />
         </a>
-        <a href="https://blog.hackillinois.org" target="_blank" rel="noopener noreferrer">
+        {/* <a href="https://blog.hackillinois.org" target="_blank" rel="noopener noreferrer">
             <img src={globe} alt="Blog Icon" />
-        </a>
+        </a> */}
         <a href="https://www.facebook.com/hackillinois/" target="_blank" rel="noopener noreferrer">
             <img src={facebook} alt="Facebook Icon" />
         </a>
@@ -26,9 +26,9 @@ const Footer = () => (
         <a href="https://twitter.com/hackillinois" target="_blank" rel="noopener noreferrer">
             <img src={twitter} alt="Twitter Icon" />
         </a>
-        <a href="mailto:contact@hackillinois.org" target="_blank" rel="noopener noreferrer">
+        {/* <a href="mailto:contact@hackillinois.org" target="_blank" rel="noopener noreferrer">
             <img src={envelope} alt="Mail Icon" />
-        </a>
+        </a> */}
     </div>
 );
 
--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -2,32 +2,32 @@ import React, { Component } from 'react'
 import { withStyles } from "@material-ui/core/styles";
 import Footer from "../Footer";
 // import logo from 'assets/logo.svg';
-import backgroundImage from 'assets/hack2020-landing-page.svg';
+import logoAndTagline from 'assets/hack_logo_and_tagline.svg';
+
+import './styles.scss';
 
 const styles = theme => ({
-  logo: {
-    width: "200px",
-    marginTop: "20px"
-  },
   background: {
-    position: "absolute",
-    left: 0,
-    top: 0,
-    zIndex: -1
+    height: "100%",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
   }
 });
 
 export class LandingPage extends Component {
   render() {
-    const { classes } = this.props;
+    // const { classes } = this.props;
 
     return (
-      <div>
-        <img className={classes.background} src={backgroundImage} alt="Background" />
-        {/* <img className={classes.logo} src={logo} alt="HackIllinois Logo" />
-        <h1>Explore. Connect. Build.</h1>
-        <h3>Feb. 28 - Mar. 1, 2020</h3> */}
-        <Footer />
+      <div className='background'>
+        {/* <img className={classes.background} src={backgroundImage} alt="Background" /> */}
+        <div className='info'>
+          <img className='logo' src={logoAndTagline} alt="Tagline and Logo" />
+          <h1>Explore. Connect. Build.</h1>
+          <h3>February 28 - March 1, 2020</h3>
+          <Footer />
+        </div>
       </div>
     )
   }
