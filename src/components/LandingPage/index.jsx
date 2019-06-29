59a185aa7bacf184a642c5cfb4581c1e1cf962ab
Brian Strauch <bstrauch24@gmail.com>
2019-06-29 03:33:05 -0500

    Refactor with improved media queries

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,39 +1,42 @@
-import React, { Component } from 'react'
-import { withStyles } from "@material-ui/core/styles";
-import Footer from "../Footer";
-import logoAndTagline from 'assets/hack_logo_and_tagline.svg';
-import bus from "assets/bus.svg";
-import car from "assets/car.svg";
+import React from 'react'
+
+import logo from 'assets/hackillinois_logo.svg';
+
+import facebook from 'assets/icons/facebook.svg';
+import github from 'assets/icons/github.svg';
+import instagram from 'assets/icons/instagram.svg';
+import twitter from 'assets/icons/twitter.svg';
 
 import './styles.scss';
 
-const styles = theme => ({
-  background: {
-    height: "100%",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "cover"
-  }
-});
-
-export class LandingPage extends Component {
+class LandingPage extends React.Component {
   render() {
-    // const { classes } = this.props;
-
     return (
-      <div className='background'>
-        {/* <img className={classes.background} src={backgroundImage} alt="Background" /> */}
-        <div className='info'>
-          <img className='logo' src={logoAndTagline} alt="Tagline and Logo" />
+      <div class="LandingPage">
+        <div class="info">
+          <img class="logo" src={logo} />
+
           <h1>Explore. Connect. Build.</h1>
-          <h3>February 28 - March 1, 2020</h3>
-          <Footer />
+          <h2>February 28 - March 1, 2020</h2>
+
+          <div class="social">
+              <a href="https://www.facebook.com/hackillinois">
+                  <img src={facebook}/>
+              </a>
+              <a href="https://www.instagram.com/hackillinois">
+                  <img src={instagram}/>
+              </a>
+              <a href="https://twitter.com/hackillinois">
+                  <img src={twitter}/>
+              </a>
+              <a href="https://www.github.com/hackillinois">
+                  <img src={github}/>
+              </a>
+          </div>
         </div>
-        <img className='bus' src={bus} alt="Bus" />
-        <img className='car' src={car} alt="Car" />
       </div>
     )
   }
 }
 
-export default withStyles(styles)(LandingPage);
+export default LandingPage;
