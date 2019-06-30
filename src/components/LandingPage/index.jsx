96c3fb6ceb05ca72fca89421bda964e1803c87e5
Brian Strauch <bstrauch24@gmail.com>
2019-06-29 22:10:49 -0500

    Initial car placement

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -7,31 +7,38 @@ import github from 'assets/icons/github.svg';
 import instagram from 'assets/icons/instagram.svg';
 import twitter from 'assets/icons/twitter.svg';
 
+import car from 'assets/car.svg';
+import bus from 'assets/bus.svg';
+
 import './styles.scss';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div class="info">
-        <img class="logo" src={logo} alt="logo" />
+      <div>
+        <div class="info">
+          <img class="logo" src={logo} alt="logo" />
 
-        <h1>Explore. Connect. Build.</h1>
-        <h2>February 28 - March 1, 2020</h2>
+          <h1>Explore. Connect. Build.</h1>
+          <h2>February 28 - March 1, 2020</h2>
 
-        <div class="social">
-            <a href="https://www.facebook.com/hackillinois">
-                <img src={facebook} alt="facebook" />
-            </a>
-            <a href="https://www.instagram.com/hackillinois">
-                <img src={instagram} alt="instagram" />
-            </a>
-            <a href="https://twitter.com/hackillinois">
-                <img src={twitter} alt="twitter" />
-            </a>
-            <a href="https://www.github.com/hackillinois">
-                <img src={github} alt="github" />
-            </a>
+          <div class="social">
+              <a href="https://www.facebook.com/hackillinois">
+                  <img src={facebook} alt="facebook" />
+              </a>
+              <a href="https://www.instagram.com/hackillinois">
+                  <img src={instagram} alt="instagram" />
+              </a>
+              <a href="https://twitter.com/hackillinois">
+                  <img src={twitter} alt="twitter" />
+              </a>
+              <a href="https://www.github.com/hackillinois">
+                  <img src={github} alt="github" />
+              </a>
+          </div>
         </div>
+
+        <img class="car" src={car} alt="car" />
       </div>
     )
   }
