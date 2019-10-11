c918ecdfab009c3b96c061720b52ae8d068b51d0
Albert Kong <akong6@illinois.edu>
2019-10-10 22:00:08 -0500

    added background image

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Styled from 'styled-components';
+import bg from 'assets/HackIllinois_temp_background.png';
 
+const Background = Styled.div`
+    background-image: url(${bg});
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 200%;
+    height: 0;
+    padding-top: 240%;
+`;
 
 export default class Home extends React.Component {
   render() {
-    return <p>/home</p>;
+    return (
+    <div>
+        <Background>
+        </Background>
+    </div>
+    );
   }
 }
