030dd5bfb34f583c971d18ad2f35aa00fabb9d0d
Albert Kong <akong6@illinois.edu>
2019-10-23 20:51:51 -0500

    background and stuff

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,23 +1,67 @@
 import React from 'react';
 import Styled from 'styled-components';
-import bg from 'assets/HackIllinois_temp_background.png';
+import bg from 'assets/svgs/background.svg';
+import tagline from 'assets/svgs/HackIllinois_Website_tagline.svg';
+import sponsor from 'assets/svgs/Sponsor.svg';
+import city from 'assets/svgs/HackIllinois_Website-05.svg';
 
-const Background = Styled.div`
-    background-image: url(${bg});
-    background-size: contain;
-    background-repeat: no-repeat;
-    width: 200%;
-    height: 0;
-    padding-top: 240%;
+const Container = Styled.div`
+  position: relative;
+`;
+
+const Background = Styled.img`
+  width: 100%;
+`;
+
+const Content = Styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+`;
+
+const Tagline = Styled.img`
+  display: block;
+  width: 40vw;
+  margin: 25vh auto 5vh auto;
+
+  @media(max-width: 900px) {
+    margin: 15vh auto 3vh auto;
+  }
+
+  /* @media(max-width: 300px) {
+    margin: 0 auto 2vh auto;
+  }
+  @media(max-width: 1000px) {
+    margin: 15vh auto 4vh auto;
+  } */
+
+
+`;
+
+const Sponsor = Styled.img`
+  display: block;
+  width: 40vw;
+  margin: 15vh auto;
+`;
+
+const City = Styled.img`
+  display: block;
+  width: 50vw;
+  margin: 15vh auto 15vh -16.5vw;
 `;
 
 export default class Home extends React.Component {
   render() {
     return (
-    <div>
-        <Background>
-        </Background>
-    </div>
+    <Container>
+        <Background src={bg} alt={'background'}/>
+        <Content>
+          <Tagline src={tagline} alt={'tagline'}/>
+          <Sponsor src={sponsor} alt={'sponsor'}/>
+          <City src={city} alt={'city'}/>
+        </Content>
+    </Container>
     );
   }
 }
