46d9a4854f4c069fb04af5b9597aaea93943178a
Abhinav Pappu <abhi1pappu9@gmail.com>
2019-10-29 16:12:20 -0500

    Rename index.js to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,5 +4,4 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import 'index.css';
 
-/* eslint-disable-next-line react/jsx-filename-extension */
 ReactDOM.render(<App />, document.getElementById('root'));
