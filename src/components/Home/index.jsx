3beca01d24ed0dfa56dc1797616d25e53229be8e
Michael Zhou <mikezhou@fulcrumgt.com>
2019-11-03 21:21:42 -0600

    fix double scrollbar issue

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import Styled, { keyframes } from 'styled-components';
+import Styled from 'styled-components';
 import bg from 'assets/svgs/background.svg';
 import tagline from 'assets/svgs/HackIllinois_Website_tagline.svg';
-import sponsor from 'assets/svgs/Sponsor.svg';
 import city from 'assets/svgs/HackIllinois_Website-05.svg';
 import logo from 'assets/svgs/HackIllinois_Website_logo.svg';
 import backdrop1 from 'assets/svgs/HackIllinois_Website-18.svg';
@@ -19,6 +18,8 @@ const BackgroundWrapper = Styled.div`
   /* display: flex; */
   align-items: center;
   justify-content: center;
+  position: absolute;
+
 `;
 const Background = Styled.img`
   width: 100%;
@@ -26,13 +27,14 @@ const Background = Styled.img`
 `;
 
 const Content = Styled.div`
-  position: absolute;
+  position: relative;
   top: 0;
   left: 0;
   width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 100vh;
 `;
 
 const SubContent = Styled.div`
@@ -49,16 +51,6 @@ const Tagline = Styled.img`
   @media(max-width: 425px){
     margin-top: 100px;
   }
-`;
-
-const Sponsor = Styled.img`
-  margin-top: 30px;
-  max-width: 600px;
-  width: 75vw;
-  display: none;
-  /* @media(max-width: 425px) {
-    display: block;
-  } */
 `;
 
 const City = Styled.img`
@@ -88,8 +80,6 @@ const City = Styled.img`
 const Backdrop1 = Styled.img`
   position: absolute;
   overflow: hidden;
-  /* flex-grow: 0;
-  flex-shrink: 0; */
   right: -300px;
   width: 90vw;
   
@@ -213,55 +203,33 @@ export default class Home extends React.Component {
               <TimeWrapper>
                 <Clickable>
                   General
-                </Clickable>
+            </Clickable>
                 <Clickable>
                   Before
-                </Clickable>
+            </Clickable>
                 <Clickable>During</Clickable>
               </TimeWrapper>
               <FAQContainer>
                 <FAQTitle>
-                  How do I get there to the University/Siebel/etc?
-                </FAQTitle>
+                  <b>
+                    How do I get there to the University of Illinois/Siebel/etc.?
+            </b><br />
+                  Hackillinois will be providing some bus routes to the event. More information on what routes the buses will take to follow. If you are a University of Illinois student please walk or utilize the MTD bus system.
+    
+            </FAQTitle>
+                <FAQTitle>
+                  <b>Do I need to have a programming background to participate?</b><br />
+                  No! People of all skill levels are welcomed.
+            </FAQTitle>
+                <FAQTitle>
+                  <b>What should I bring?</b> <br />
+                  You should bring a student ID, a reusable water bottle, a change of clothing, personal items such as toiletries, and a laptop & charger. Due to safety considerations, please do not bring desktop computers, extra monitors, weapons, or alcoholic beverages.
+            </FAQTitle>
               </FAQContainer>
             </GroundContent>
           </SubContent>
         </Content>
-<<<<<<< HEAD
-        <GroundContent>
-          <RoadWrapper>
-            <Road src={bottomroad} />
-          </RoadWrapper>
-          <TimeWrapper>
-            <Clickable>
-              General
-            </Clickable>
-            <Clickable>
-              Before
-            </Clickable>
-            <Clickable>During</Clickable>
-          </TimeWrapper>
-          <FAQContainer>
-            <FAQTitle>
-              <b>
-                How do I get there to the University of Illinois/Siebel/etc.?
-            </b><br />
-              Hackillinois will be providing some bus routes to the event. More information on what routes the buses will take to follow. If you are a University of Illinois student please walk or utilize the MTD bus system.
 
-            </FAQTitle>
-            <FAQTitle>
-            <b>Do I need to have a programming background to participate?</b><br/>
-No! People of all skill levels are welcomed. 
-            </FAQTitle>
-            <FAQTitle>
-            <b>What should I bring?</b> <br/>
-You should bring a student ID, a reusable water bottle, a change of clothing, personal items such as toiletries, and a laptop & charger. Due to safety considerations, please do not bring desktop computers, extra monitors, weapons, or alcoholic beverages.
-</FAQTitle>
-          </FAQContainer>
-        </GroundContent>
-=======
-        
->>>>>>> 6c1569e2273670d896b9d35ad81905e3cc4e7e13
       </Container>
     );
   }
