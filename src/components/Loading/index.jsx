0e950db04880b7d11e12a6cdc4da9a0e0acac941
Brian Strauch <bstrauch24@gmail.com>
2019-12-24 14:48:25 -0600

    Loading animation

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+
+import animationData from 'assets/car.json';
+import './style.scss';
 
 export default function Loading() {
-  return <p>Loading...</p>;
+  const options = {
+    animationData: animationData,
+    autoplay: true,
+    loop: true,
+  };
+
+  const style = {
+    marginRight: '15vw',
+    width: '80vw',
+    height: '80vh',
+  };
+  
+  return (
+    <div id="Loading">
+      <Lottie id="car" options={options} style={style} />
+      <h1>Loading...</h1>
+    </div>
+  );
 }
