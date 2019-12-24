5f36430ad8758b4c426d3af4841ece1536bfc240
Brian Strauch <bstrauch24@gmail.com>
2019-12-24 14:54:36 -0600

    Resolved lint warnings

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -5,18 +5,9 @@ import animationData from 'assets/car.json';
 import './style.scss';
 
 export default function Loading() {
-  const options = {
-    animationData: animationData,
-    autoplay: true,
-    loop: true,
-  };
+  const options = { animationData, autoplay: true, loop: true };
+  const style = { marginRight: '15vw', width: '80vw', height: '80vh' };
 
-  const style = {
-    marginRight: '15vw',
-    width: '80vw',
-    height: '80vh',
-  };
-  
   return (
     <div id="Loading">
       <Lottie id="car" options={options} style={style} />
