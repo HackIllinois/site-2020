01e7ba8a8a71b751cea629b5eef2d9c55eca2938
Muhammed Imran <imranmuhammed2000@gmail.com>
2020-01-16 12:53:58 -0600

    copied dashboard from old branch to new branch. matches dev file structure.

---
+++ src/scenes/Dashboard/cells/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import hackillinoislogo from 'assets/logo.svg';
+
+function Logo(props, context) {
+  return (
+    <div className={`cell short-cell ${ context}`} id="logo-cell">
+      <img src={hackillinoislogo} id="logo" alt="HackIllinois Logo" />
+      <h1>Explore. <br /> Connect. <br /> Build. </h1>
+    </div>
+  );
+}
+
+export default Logo;
