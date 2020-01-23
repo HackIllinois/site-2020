48ff30ef2f6473f43096f47f013d8217127ad740
Michael Zhou <mikezhou@fulcrumgt.com>
2020-01-22 22:16:00 -0600

    added base template for the schedule

--- src/scenes/Schedule/index.jsx
+++ src/scenes/Schedule/index.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import Backdrop from 'components/Backdrop';
+import './styles.scss';
 
 export default class Schedule extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentSection: 0,
+      events: [],
     };
   }
 
+  componentDidMount() {
+    this.getEvents();
+  }
+
+  getEvents() {
+    fetch('https://api.hackillinois.org/event/', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(res => {
+      res.json().then(data => {
+        this.setState({
+          events: data.events,
+        });
+      });
+    });
+  }
+
+  /*
+  description
+  startTime
+  endTime
+  sponsor
+  description
+  eventType
+  name
+  */
   render() {
     const { currentSection } = this.state;
-
     return (
       <div>
         <Backdrop
@@ -20,9 +50,28 @@ export default class Schedule extends React.Component {
           selectedSign={currentSection}
           onSignClick={signIndex => this.setState({ currentSection: signIndex })}
         >
-          <div style={{ width: '100%', height: '100%', backgroundColor: '#E28B79' }}>
-            Placeholder to demonstrate available space for content
+          <div className="whole-sign">
+            <div className="top-bar" />
+            <div className="leg-supports">
+              <div className="left-leg">
+                <div className="shade" />
+              </div>
+              <div className="right-leg">
+                <div className="shade" />
+              </div>
+            </div>
+            <div className="display-board">
+              <div className="display-internal">
+                {this.state.events.map(e => (
+                  <div>{e.name}</div>
+                ))}
+              </div>
+            </div>
+            <div className="sign-leg">
+              <div className="shade" />
+            </div>
           </div>
+
         </Backdrop>
       </div>
     );
