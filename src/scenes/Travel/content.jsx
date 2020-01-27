7d2066d5d57ac80373634fb2f9c5a5bef0d08de6
Albert Kong <akong6@illinois.edu>
2020-01-26 18:29:41 -0600

    added some billboard text and stuff

---
+++ src/scenes/Travel/content.jsx
@@ -0,0 +1,134 @@
+const Illinois = [
+  {
+    title: 'TO UIUC',
+    body: [
+      {
+        title: 'PICKUP',
+        body: [
+          {
+            title: 'LAMO - 2/22/2019 at 9:00AM EST',
+            body: 'Electrical Engineering and Computer Science Building\n1301 Beal Ave, Ann Arbor, MI 48109',
+          },
+          {
+            title: 'LAMOOOOO - 2/22/2019 at 10:30AM EST',
+            body: 'Engineering Building\n428 S Shaw Ln #3115, East Lansing, MI 48824',
+          },
+        ],
+      },
+      {
+        title: 'ARRIVAL',
+        body: [
+          {
+            title: 'YEEE - 4:15PM CST',
+            body: 'Siebel Center for Computer Science\n201 N Goodwin Ave, Urbana, IL 61801',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'FROM UIUC',
+    body: [
+      {
+        title: 'PICKUP',
+        body: [
+          {
+            title: '2/24/2019 at 5:15PM CST',
+            body: '',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Michigan = [
+  {
+    title: 'TO UIUC',
+    body: [
+      {
+        title: 'PICKUP',
+        body: [
+          {
+            title: 'UMICHIGAN - 2/22/2019 at 9:00AM EST',
+            body: 'Electrical Engineering and Computer Science Building\n1301 Beal Ave, Ann Arbor, MI 48109',
+          },
+          {
+            title: 'MICHIGAN STATE U - 2/22/2019 at 10:30AM EST',
+            body: 'Engineering Building\n428 S Shaw Ln #3115, East Lansing, MI 48824',
+          },
+        ],
+      },
+      {
+        title: 'ARRIVAL',
+        body: [
+          {
+            title: '4:15PM CST',
+            body: 'Siebel Center for Computer Science\n201 N Goodwin Ave, Urbana, IL 61801',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'FROM UIUC',
+    body: [
+      {
+        title: 'PICKUP',
+        body: [
+          {
+            title: '2/24/2019 at 5:15PM CST',
+            body: '',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Indiana = [
+  {
+    title: 'TO UIUC',
+    body: [
+      {
+        title: 'PICKUP',
+        body: [
+          {
+            title: 'INDIANAAAA - 2/22/2019 at 9:00AM EST',
+            body: 'Electrical Engineering and Computer Science Building\n1301 Beal Ave, Ann Arbor, MI 48109',
+          },
+          {
+            title: 'MICHIGAN STATE U - 2/22/2019 at 10:30AM EST',
+            body: 'Engineering Building\n428 S Shaw Ln #3115, East Lansing, MI 48824',
+          },
+        ],
+      },
+      {
+        title: 'ARRIVAL',
+        body: [
+          {
+            title: 'YAA 4:15PM CST',
+            body: 'Siebel Center for Computer Science\n201 N Goodwin Ave, Urbana, IL 61801',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'FROM UIUC',
+    body: [
+      {
+        title: 'PICKUP',
+        body: [
+          {
+            title: '2/24/2019 at 5:15PM CST',
+            body: '',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const Locations = [Illinois, Michigan, Indiana];
+export default Locations;
--- src/scenes/Travel/index.jsx
+++ src/scenes/Travel/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Backdrop from 'components/Backdrop';
 import BillboardTop from 'assets/travel/travel_billboard_top.svg';
+import CONTENT from './content';
 import './style.scss';
 
 export default class Travel extends React.Component {
@@ -9,16 +10,22 @@ export default class Travel extends React.Component {
     this.state = {
       currentSection: 0,
     };
+
+    this.handleLocation = this.handleLocation.bind(this);
+  }
+
+  handleLocation(e) {
+    this.setState({ currentSection: e.target.value });
   }
 
   render() {
     const { currentSection } = this.state;
-
+    const LOCATIONS = ['Illinois', 'Michigan', 'Indiana'];
     return (
       <div>
         <Backdrop
           title="Transport"
-          signs={['Illinois', 'Michigan', 'Indiana']}
+          signs={LOCATIONS}
           selectedSign={currentSection}
           onSignClick={signIndex => this.setState({ currentSection: signIndex })}
         >
@@ -27,7 +34,32 @@ export default class Travel extends React.Component {
             <div className="billboard-bottom">
               <div className="leg" />
               <div className="sign-container">
-                <div className="text-container" />
+                <select onChange={this.handleLocation} value={currentSection} name="locations">
+                  {LOCATIONS.map((e, i) => (
+                    <option key={e} value={i}>{e}</option>
+                  ))}
+                </select>
+                <div className="text-container">
+                  {CONTENT[currentSection].map((e, i) => (
+                    <div key={e} className="section-container">
+                      {i !== 0 && <hr />}
+                      <h3>{e.title}</h3>
+                      {e.body.map(f => (
+                        <div key={f} className="sub-container">
+                          <div className="left-column">{f.title}</div>
+                          <div className="item-container">
+                            {f.body.map(g => (
+                              <div key={g} className="item">
+                                <h4>{g.title}</h4>
+                                <p>{g.body}</p>
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+                </div>
                 <div className="stando" />
               </div>
             </div>
