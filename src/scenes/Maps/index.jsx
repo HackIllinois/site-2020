590a10fbc4a4d3e790be4b1008fbc50361ecd5b0
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-01-26 21:26:58 -0600

    Add map assets and initial version of dropdown

--- src/scenes/Maps/index.jsx
+++ src/scenes/Maps/index.jsx
@@ -3,6 +3,7 @@ import Backdrop from 'components/Backdrop';
 
 import topStructure from 'assets/maps/top_structure.svg';
 import './style.scss';
+import Select from 'react-select';
 
 export default class Maps extends React.Component {
   constructor(props) {
@@ -15,10 +16,51 @@ export default class Maps extends React.Component {
   render() {
     const { currentSection } = this.state;
 
+    const locations = [
+      { name: 'DCL', maps: [] },
+      { name: 'Siebel', maps: [] },
+      { name: 'ECEB', maps: [] },
+      { name: 'Kenney', maps: [] },
+    ];
+
+    const customSelectStyles = {
+      control: provided => ({
+        ...provided,
+        backgroundColor: '#E26856',
+        border: 'none',
+        boxShadow: 'none',
+        borderRadius: '0',
+      }),
+      singleValue: provided => ({
+        ...provided,
+        color: 'white',
+      }),
+      indicatorSeparator: () => ({
+        display: 'none',
+      }),
+      dropdownIndicator: provided => ({
+        ...provided,
+        color: 'white !important', // !important is to prevent color change on hover
+      }),
+      input: provided => ({
+        ...provided,
+        color: 'white',
+      }),
+      menuList: provided => ({
+        ...provided,
+        backgroundColor: '#E26856',
+      }),
+      option: (provided, { isSelected }) => ({
+        ...provided,
+        backgroundColor: isSelected ? '#A14234 !important' : '#E26856 !important',
+        color: 'white',
+      }),
+    };
+
     return (
       <Backdrop
         title="Maps"
-        signs={['DCL', 'Siebel', 'ECEB', 'Kenney']}
+        signs={locations.map(location => location.name)}
         selectedSign={currentSection}
         onSignClick={signIndex => this.setState({ currentSection: signIndex })}
       >
@@ -31,9 +73,15 @@ export default class Maps extends React.Component {
             <div className="first connector" />
             <div className="last connector" />
 
-            <div className="maps-container">
-              
-            </div>
+            <div className="maps-container"></div>
+
+            <Select
+              className="location-select"
+              value={{ label: locations[currentSection].name, value: currentSection }}
+              onChange={selected => this.setState({ currentSection: selected.value })}
+              options={locations.map((location, index) => ({ label: location.name, value: index }))}
+              styles={customSelectStyles}
+            />
           </div>
         </div>
       </Backdrop>
