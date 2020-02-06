26b0f9eb870775429e79afe8de5378d5f833a9da
Joan Ball <prcsn.lvr@gmail.com>
2020-02-06 14:28:27 -0600

    add billboard, content, and storefront svg

--- src/scenes/Prizes/index.jsx
+++ src/scenes/Prizes/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import Backdrop from 'components/Backdrop';
 import { getPrizes } from 'api';
+
+import prizes_front from 'assets/common_backdrop/prizes-front.svg';
+
+import './style.scss';
 
 export default class Prizes extends React.Component {
   constructor(props) {
@@ -19,15 +24,33 @@ export default class Prizes extends React.Component {
     const prizesList = this.state.prizes;
 
     return (
-      <div className="prizes">
-        {prizesList.map(prize => (
-          <div className="prize">
-            <h1>{prize.name}</h1>
-            <h2>Sponsored by: {prize.sponsor}</h2>
-            <p>{prize.description}</p>
+      <Backdrop
+        title="Prizes"
+        signs={[]}
+      >
+        <div className="scene-content">
+          <img className="prizes-front" src={prizes_front} />
+          <div className="billboard-container">
+            <div className="pole" id="left"></div>
+            <div className="left top connector"></div>
+            <div className="left bottom connector"></div>
+            <div className="pole" id="right"></div>
+            <div className="right top connector"></div>
+            <div className="right bottom connector"></div>
+
+            <div className="billboard">
+              {prizesList.map(prize => (
+                <div className="prize">
+                  <p className="prize-name">{prize.name}</p>
+                  <p className="prize-sponsor">Sponsored by: {prize.sponsor}</p>
+                  <p>{prize.description}</p>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
+
+        </div>
+      </Backdrop>
     );
   }
 }
