e39ce9dddc891c11c13bf6d6640cd9c504cc6f77
Joan Ball <prcsn.lvr@gmail.com>
2020-02-19 23:48:51 -0600

    starter code and rules added

--- src/scenes/Editor/index.jsx
+++ src/scenes/Editor/index.jsx
@@ -7,20 +7,53 @@ import "ace-builds/src-noconflict/theme-monokai";
 
 export default class Editor extends React.Component {
   render() {
+    const starterCode = `<html>
+  <head>
+    <title>Code in the Dark</title>
+
+    <style>
+
+    </style>
+  </head>
+
+  <body>
+
+  </body>
+</html>
+`;
+
     return (
       <div>
-        <AceEditor
-          mode="html"
-          theme="monokai"
-          showGutter={true}
-          highlightActiveLine={true}
-          tabSize={2}
-          setOptions={{
-            showLineNumbers: true,
-            useSoftTabs: true,
-            fontFamily: 'monospace',
-          }}
-        />
+        <h1>Code in the Dark!</h1>
+
+        <div class="content">
+          <AceEditor
+            mode="html"
+            theme="monokai"
+            showGutter={true}
+            highlightActiveLine={true}
+            tabSize={2}
+            defaultValue={starterCode}
+            setOptions={{
+              showLineNumbers: true,
+              useSoftTabs: true,
+            }}
+          />
+
+          <div class="rules">
+            <h3>Rules:</h3>
+            <ol>
+              <li>Do not edit this code until we tell you to</li>
+              <li>Do not navigate away from this tab!</li>
+              <li>Do not preview your HTML code</li>
+            </ol>
+
+            <h3>Submission:</h3>
+            <p>Name:</p>
+            <p>Problem Number:</p>
+            <p>Submit</p>
+          </div>
+        </div>
       </div>
     );
   }
