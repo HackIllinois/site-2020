51e5f46702abf0a53ca46fc75f46470bd8008e73
Joan Ball <prcsn.lvr@gmail.com>
2020-02-24 21:13:12 -0600

    satisfy linter

--- src/scenes/Editor/index.jsx
+++ src/scenes/Editor/index.jsx
@@ -3,8 +3,8 @@ import AceEditor from 'react-ace';
 import emailjs from 'emailjs-com';
 import './style.scss';
 
-import "ace-builds/src-noconflict/mode-html";
-import "ace-builds/src-noconflict/theme-monokai";
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/theme-monokai';
 
 export default class Editor extends React.Component {
   constructor(props) {
@@ -26,35 +26,32 @@ export default class Editor extends React.Component {
 `;
 
     this.state = {
-      name: "",
-      problem: "",
+      name: '',
+      problem: '',
       codeValue: starterCode,
       changeSinceSubmit: true,
       successfulSubmit: true,
-      statusMessage: "",
-    }
+      statusMessage: '',
+    };
   }
 
   onChange = event => {
-    this.setState({ 
-      ...this.state,
+    this.setState({
       [event.target.name]: event.target.value,
     });
   }
 
   onEditorChange = newCode => {
     this.setState({
-      ...this.state,
       changeSinceSubmit: true,
       codeValue: newCode,
     });
   }
-  
+
   submit = () => {
     if (!this.state.changeSinceSubmit && this.state.successfulSubmit) {
       this.setState({
-        ...this.state,
-        statusMessage: "No changes since last submit",
+        statusMessage: 'No changes since last submit',
       });
 
       return;
@@ -64,21 +61,19 @@ export default class Editor extends React.Component {
       name: this.state.name,
       problem: this.state.problem,
       message_body: this.state.codeValue,
-    }
+    };
 
     const templateId = process.env.REACT_APP_EMAIL_TEMPLATE;
     const userId = process.env.REACT_APP_EMAIL_USER;
 
     emailjs.send('gmail', templateId, params, userId)
-      .then((result) => {
+      .then(() => {
         this.setState({
-          ...this.state,
           changeSinceSubmit: false,
-          statusMessage: "Successful submission",
+          statusMessage: 'Successful submission',
         });
-      }, (error) => {
+      }, error => {
         this.setState({
-          ...this.state,
           changeSinceSubmit: false,
           statusMessage: error,
         });
@@ -86,19 +81,25 @@ export default class Editor extends React.Component {
   }
 
   render() {
-    const { statusMessage, changeSinceSubmit, successfulSubmit, codeValue } = this.state;
+    const {
+      statusMessage,
+      changeSinceSubmit,
+      successfulSubmit,
+      codeValue,
+    } = this.state;
+
     const displayStatus = !successfulSubmit || !changeSinceSubmit;
 
     return (
       <div>
         <h1>Code in the Dark!</h1>
 
-        <div class="content">
+        <div className="content">
           <AceEditor
             mode="html"
             theme="monokai"
-            showGutter={true}
-            highlightActiveLine={true}
+            showGutter
+            highlightActiveLine
             tabSize={2}
             value={codeValue}
             onChange={this.onEditorChange}
@@ -108,7 +109,7 @@ export default class Editor extends React.Component {
             }}
           />
 
-          <div class="rules">
+          <div className="rules">
             <h3>Rules:</h3>
             <ol>
               <li>Do not edit this code until we tell you to</li>
@@ -118,17 +119,17 @@ export default class Editor extends React.Component {
 
             <h3>Tips:</h3>
             <ul>
-              <li>Our logo can be found at "assets/logo.png"</li>
+              <li>Our logo can be found at &quot;assets/logo.png&quot;</li>
             </ul>
 
             <h3>Submission:</h3>
             <div className="submit">
-              <p class={displayStatus ? "status" : "status inactive"}>
+              <p className={displayStatus ? 'status' : 'status inactive'}>
                 {statusMessage}
               </p>
-              <label for="name-input">Team Name:</label>
+              <p>Team Name:</p>
               <input type="text" id="name-input" name="name" onChange={this.onChange} />
-              <label for="problem-input">Problem Number:</label>
+              <p>Problem Number:</p>
               <input type="text" id="problem-input" name="problem" onChange={this.onChange} />
               <button type="button" onClick={this.submit}>Submit</button>
             </div>
