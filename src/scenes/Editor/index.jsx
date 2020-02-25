ec7189422bfba0d8082a68982eee277e0aa34db7
Joan Ball <prcsn.lvr@gmail.com>
2020-02-24 20:17:48 -0600

    add submission form
    hiding api keys/ids

--- src/scenes/Editor/index.jsx
+++ src/scenes/Editor/index.jsx
@@ -7,7 +7,9 @@ import "ace-builds/src-noconflict/mode-html";
 import "ace-builds/src-noconflict/theme-monokai";
 
 export default class Editor extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
     const starterCode = `<html>
   <head>
     <title>Code in the Dark</title>
@@ -23,6 +25,45 @@ export default class Editor extends React.Component {
 </html>
 `;
 
+    this.state = {
+      name: "",
+      problem: "",
+      codeValue: starterCode,
+    }
+  }
+
+  onChange = event => {
+    console.log(event)
+    this.setState({ 
+      ...this.state,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  onEditorChange = newCode => {
+    this.setState({
+      codeValue: newCode
+    });
+  }
+  
+  submit = () => {
+    const params = {
+      name: this.state.name,
+      problem: this.state.problem,
+      message_body: this.state.codeValue,
+    }
+
+    emailjs.send('gmail', /*template_id*/, params, /*user_id*/)
+      .then((result) => {
+        console.log(result.test);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
+  render() {
+    const { name, problem, codeValue } = this.state;
+
     return (
       <div>
         <h1>Code in the Dark!</h1>
@@ -34,7 +75,8 @@ export default class Editor extends React.Component {
             showGutter={true}
             highlightActiveLine={true}
             tabSize={2}
-            defaultValue={starterCode}
+            value={codeValue}
+            onChange={this.onEditorChange}
             setOptions={{
               showLineNumbers: true,
               useSoftTabs: true,
@@ -49,10 +91,19 @@ export default class Editor extends React.Component {
               <li>Do not preview your HTML code</li>
             </ol>
 
+            <h3>Tips:</h3>
+            <ul>
+              <li>Our logo can be found at "assets/logo.png"</li>
+            </ul>
+
             <h3>Submission:</h3>
-            <p>Name:</p>
-            <p>Problem Number:</p>
-            <p>Submit</p>
+            <div className="submit">
+              <label for="name-input">Team Name:</label>
+              <input type="text" id="name-input" name="name" onChange={this.onChange} />
+              <label for="problem-input">Problem Number:</label>
+              <input type="text" id="problem-input" name="problem" onChange={this.onChange} />
+              <button type="button" onClick={this.submit}>Submit</button>
+            </div>
           </div>
         </div>
       </div>
